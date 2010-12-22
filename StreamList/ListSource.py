5d02ac60f92f9dbba8f7e4b3f34b97b5b22e9366
deflis <deflis@gmail.com>
2010-12-23 03:12:40 +0900

    Add Comment

--- StreamList/ListSource.py
+++ StreamList/ListSource.py
@@ -5,6 +5,7 @@ from Solar import *
 from Solar.Models import *
 from NekoVampire import StreamList
 
+# ユーザー名とリスト名に自分の使いたいリストを入れてください。（@ユーザー名/リスト名）
 sl = StreamList("ユーザー名", "リスト名")
 
 def GetStatuses(client, range):
